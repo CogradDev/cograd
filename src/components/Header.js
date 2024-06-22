ac2b45a94b1658b77438c39823a66673a5013e01
sumit nirmal <sumitramprakashnirmal@gmail.com>
2024-06-22 08:16:41 +0530

    first

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/cograd-black-logo.webp";
 import "./Header.scss";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,25 +21,42 @@ const Header = () => {
     };
   }, [isScrolled]);
 
-  console.log(isScrolled)
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <header className={`header ${!isScrolled ? "fixed" : ""}`}>
-      <div className="logo">
-        <img src={logo} alt="Cograd Logo" />
+    <>
+      <header className={`header ${isScrolled ? "scrolled" : ""}`}>
+        <div className="logo">
+          <img src={logo} alt="Cograd Logo" />
+        </div>
+        <nav className="navigation">
+          <a href="#home">Home</a>
+          <a href="#about-us">About Us</a>
+          <a href="#services">Services</a>
+          <a href="#resources">Resources</a>
+        </nav>
+        <div className="button-container">
+          <a href="#contact-us" className="contact-button">
+            Contact Us
+          </a>
+        </div>
+        <div className="hamburger" onClick={toggleSidebar}>
+          <FaBars />
+        </div>
+      </header>
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="sidebar-content">
+          <a href="#home" onClick={toggleSidebar}>Home</a>
+          <a href="#about-us" onClick={toggleSidebar}>About Us</a>
+          <a href="#services" onClick={toggleSidebar}>Services</a>
+          <a href="#resources" onClick={toggleSidebar}>Resources</a>
+          <a href="#contact-us" onClick={toggleSidebar}>Contact Us</a>
+        </div>
       </div>
-      <nav className="navigation">
-        <a href="#home">Home</a>
-        <a href="#about-us">About Us</a>
-        <a href="#services">Services</a>
-        <a href="#resources">Resources</a>
-      </nav>
-      <div className="button-container">
-        <span href="#contact-us" className="contact-button">
-          Contact Us
-        </span>
-      </div>
-    </header>
+      {isSidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
+    </>
   );
 };
 
